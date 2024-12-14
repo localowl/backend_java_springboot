f8d15fcde32639f59e80765d4a16f9a3a81afdf8
localowl <mervekeser8@outlook.com>
2024-12-14 17:26:25 +0300

    crud demo start

---
+++ spring-boot-hibernate-jpa-crud/1-crud-demo/src/test/java/com/localowl/crud_demo/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localowl.crud_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
