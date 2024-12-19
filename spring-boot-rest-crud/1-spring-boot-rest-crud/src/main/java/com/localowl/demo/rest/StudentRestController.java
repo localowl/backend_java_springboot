4d8e94e6abe4777e57b9709646c21f9de8bb76b8
localowl <mervekeser8@outlook.com>
2024-12-19 12:47:32 +0300

    rest path variables

--- spring-boot-rest-crud/1-spring-boot-rest-crud/src/main/java/com/localowl/demo/rest/StudentRestController.java
+++ spring-boot-rest-crud/1-spring-boot-rest-crud/src/main/java/com/localowl/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.localowl.demo.rest;
 
 import com.localowl.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1")
 public class StudentRestController {
-    //define endpoints for "/students"
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    //define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Kath", "Lock"));
         theStudents.add(new Student("Kylie", "Jack"));
-
+    }
+    //define endpoints for "/students"
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
+    }
+
+    //define endpoints "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 }
